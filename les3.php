1e0bcd416d9601095c835f5e9eaaa9881a6b2057
mno-davinci <mnouwen@davinci.nl>
2015-03-09 09:43:57 +0100

    Les maandag

---
+++ les3.php
@@ -0,0 +1,31 @@
+<?php
+
+$bla = "bla"; 	//statements worden afgesloten met een ;
+
+$int = 10; // variable datatype is een integer (hele getalen, 1, 2 ,3 etc.)
+$boolean = true; //variable datatype is een boolean (true of false)
+$float = 1.29; //variable datatype is een float (getallen met 'precisie' achter komma)
+$string = "dit is een string"; //variable datatype is een string (reeks karakters)
+
+$assoc_array['waarde'] = "dit is een waarde in de array"; //voorbeeld assoc array
+$assoc_array['waarde2'] = "dit is ook een waarde in de array"; //voorbeeld assoc array
+
+$num_array[1] = "dit is een waarde in de array"; //voorbeeld numeric array
+$num_array[2] = "dit is ook een waarde in de array"; //voorbeeld numeric array
+
+$num_assoc_array[0]['naam'] = "naam"; //multidimensionale array
+$num_assoc_array[0]['achternaam'] = "achternaam"; //multidimensionale array
+$num_assoc_array[0]['programmeur'] = false;
+
+$num_array = array("waarde1", "waarde2", "waarde3"); //voorbeeld numeric array
+$num_array = array(1, 2, 3);
+
+$assoc_array = array("voornaam"=>"Henk", "achternaam"=>"Potlood"); //voorbeeld assoc array
+
+$num_assoc_array = array(array("voornaam"=>"Henk", "achternaam"=>"potlood"), array());
+
+if (1 == 1) {		//if is een keyword. if maakt gebruik van condities om het codeblock uit te voeren.
+	# code...
+}
+
+?>
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
-Hoi!
+#14A1 repo
+Welkom op de 14A1 repo!
