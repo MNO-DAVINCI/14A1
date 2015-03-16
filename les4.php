97f11f901540129d1cbf32da186ad39cb582dff2
mno-davinci <mnouwen@davinci.nl>
2015-03-16 09:48:25 +0100

    Nieuwe les

---
+++ les4.php
@@ -0,0 +1,44 @@
+<?php
+/*
+	modulus, bereken de rest waarde
+*/
+$uitkomst = 7%2; //uitkomst = 1
+$uitkomst2 = 9%3; //uitkomst = 0
+$uitkomst3 = 11%3; //uitkomst = 2
+
+$x = 10;
+$y = "10";
+
+if($x === $y)
+{
+	echo "True";
+} else {
+	echo "False";
+}
+
+$x = !true;
+
+$x = 10;
+
+if (!is_string($x));
+
+echo $x . "<br>";
+
+while(true) {
+
+
+	if($exit == true) {
+		break;
+	}
+}
+
+for ($i=0; $i <= 10 ; $i++) { 
+	
+	if($i == 5) {
+		continue 2;
+	}
+
+	echo $i;
+}
+
+?>
